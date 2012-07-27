757d9c25c8bc5b1e4d5a8a313ee200d787ecbe0d
cpraveen <cpraveen@30a28bc5-c0d8-4b65-9caa-fb6438b53d1d>
2012-07-27 09:09:01 +0000

    nitsche_pen should always be specified, even for strong bc, in which case it is ignored.

--- src/parameter.cc
+++ src/parameter.cc
@@ -152,12 +152,9 @@ void Parameter::read_numeric (Reader &fin)
       exit (0);
    }
 
-   if(bc_scheme == Parameter::weak)
-   {
-      fin.entry ("nitsche_pen");
-      fin >> Cpen;
-      assert (Cpen >= 0.0);
-   }
+   fin.entry ("nitsche_pen");
+   fin >> Cpen;
+   assert (Cpen >= 0.0);
 
    fin.end_section ();
 
