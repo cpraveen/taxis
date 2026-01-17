bc1647b88dd4d865fc55b1a4b71867b1e77a815e
Praveen C <cpraveen@gmail.com>
2026-01-17 11:04:58 +0530

    Fix check if STOP file exists

--- src/check_for_stop_file.cc
+++ src/check_for_stop_file.cc
@@ -6,7 +6,7 @@ using namespace std;
 bool check_for_stop_file ()
 {
    ifstream ifile("STOP");
-   if(ifile)
+   if(ifile.good())
       cout << "Found STOP file !!!\n";
-   return ifile;
+   return ifile.good();
 }
